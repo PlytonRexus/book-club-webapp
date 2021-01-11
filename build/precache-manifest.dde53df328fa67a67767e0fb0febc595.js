f57dc8954e0c5655226970ebd53913b8d15b0b83
Mihir Jichkar <j.mihir19.mj@gmail.com>
2021-01-12 00:18:30 +0530

    added build folder

---
+++ build/precache-manifest.dde53df328fa67a67767e0fb0febc595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3deb89bef7620d0ed201e6e36e9b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c971e46f23cbaa368276",
+    "url": "/static/css/main.aab0ce8c.chunk.css"
+  },
+  {
+    "revision": "21f14a7582e5c5837f81",
+    "url": "/static/js/2.037a6c55.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.037a6c55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c971e46f23cbaa368276",
+    "url": "/static/js/main.ce83da47.chunk.js"
+  },
+  {
+    "revision": "7505a1d6e7797247ed01",
+    "url": "/static/js/runtime-main.d8e43a8d.js"
+  }
+]);
